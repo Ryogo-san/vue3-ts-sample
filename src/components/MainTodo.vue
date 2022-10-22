8d8f9a65c744d0ced541ca59657b648fd624b559
Ryogo-san <ritosetsugekka0226@gmail.com>
2022-10-22 14:04:45 +0900

    bind confirm

---
+++ src/components/MainTodo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="box_input">
+        <input
+            type="text"
+            class="todo_input"
+            v-model="todoRef"
+            placeholder="＋ TODOを入力"
+        />
+        <div>{{ todoRef }}</div>
+        <button class="btn">追加</button>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from "vue";
+
+    const todoRef = ref("");
+</script>
+
+<style scoped>
+    .box_input {
+        margin-top: 20px;
+    }
+
+    .todo_input {
+        width: 300px;
+        margin-right: 8px;
+        padding: 8px;
+        font-size: 18px;
+        border: 1px solid #aaa;
+        border-radius: 6px;
+    }
+
+    .btn {
+        padding: 8px;
+        background-color: #03a9f4;
+        border-radius: 6px;
+        color: #fff;
+        text-align: center;
+        font-size: 14px;
+    }
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="title">TODO</h1>
+</template>
+
+<style scoped>
+    .title {
+        width: 100%;
+        background-color: #00cc00;
+        text-align: center;
+        font-size: 32px;
+        font-weight: bold;
+        padding: 8px 0;
+    }
+</style>
