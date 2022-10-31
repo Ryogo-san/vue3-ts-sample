af4eff3df0dc4e6683152761d32a4b84a23458db
Ryogo-san <ritosetsugekka0226@gmail.com>
2022-10-31 21:37:51 +0900

    [add]ローカルストレージアクセス

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -9,6 +9,18 @@
         <div>{{ todoRef }}</div>
         <button class="btn" @click="addTodo">追加</button>
     </div>
+    <div class="box_list">
+        <div class="todo_list" v-for="todo in todoListRef" :key="todo.id">
+            <div class="todo">
+                <input type="checkbox" class="check" />
+                <label>{{ todo.task }}</label>
+            </div>
+            <div class="btns">
+                <button class="btn green">編</button>
+                <button class="btn pink">削</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -16,6 +28,8 @@
 
     const todoRef = ref("");
     const todoListRef = ref([]);
+    const ls = localStorage.todoList;
+    todoListRef.value = ls ? JSON.parse(ls) : [];
 
     const addTodo = () => {
         // IDを簡易的にミリ秒で登録する
@@ -36,6 +50,15 @@
 <style scoped>
     .box_input {
         margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+    }
+
+    .todo_list {
+        display: flex;
+        align-items: center;
+        gap: 8px;
     }
 
     .todo_input {
@@ -54,5 +77,38 @@
         color: #fff;
         text-align: center;
         font-size: 14px;
+    }
+
+    .box_list {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+    }
+
+    .todo {
+        border: 1px solid #ccc;
+        border-radius: 6px;
+        padding: 12px;
+        width: 300px;
+    }
+
+    .check {
+        border: 1px solid red;
+        transform: scale(1.6);
+        margin: 0 16px 2px 6px;
+    }
+
+    .btns {
+        display: flex;
+        gap: 4px;
+    }
+
+    .green {
+        background-color: #00c853;
+    }
+
+    .pink {
+        background-color: #ff4081;
     }
 </style>
